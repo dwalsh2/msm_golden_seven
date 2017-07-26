6f8ca904f47671293232f82264596b3976c5f925
Dan Walsh <dwalsh2@chicagobooth.edu>
2017-07-26 02:56:26 +0000

    partial solution to directors

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,70 @@
+class DirectorsController < ApplicationController
+    def new_form
+        
+        render("directors/new_form.html.erb")
+    end
+    
+    
+    def create_row
+      
+      @director = Director.new
+      @director.name = params["director_name"]
+      @director.bio = params["director_bio"]
+      @director.dob = params["director_dob"]
+      @director.image_url = params["director_url"]
+
+      @director.save
+      
+      redirect_to("/directors")
+    end
+    
+    
+    def index
+        
+      @our_directors = Director.order(:created_at => :desc)
+            
+      render("directors/index.html.erb")
+    end
+    
+    
+    def show
+        
+      @director = Director.find(params["id"])
+        
+      render("directors/show.html.erb")
+    end
+    
+   
+    def edit_form
+      
+      @director = Director.find(params["id"])
+      
+      render("directors/edit_form.html.erb")
+    end
+    
+    def update_row
+      
+      @director = Director.find(params["id"])
+
+      @director.dob = params[":dob"]
+      @director.name = params[":name"]
+      @director.bio = params[":bio"]
+      @director.image_url = params[":image_url"]
+
+      @director.save
+
+      redirect_to("/directors")
+    end
+    
+    def destroy_row
+      
+      @director = Director.find(params["id"])
+
+      @director.destroy
+      
+      render("directors/destroy_row.html.erb")
+    end
+    
+    
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,61 @@
 Rails.application.routes.draw do
+  
+# for homepage
+get("/", { :controller => "movies", :action => "index" })
+
+# to access Directors
+# CREATE
+get("/directors/new_form", { :controller => "directors", :action => "new_form" })
+get("/create_director", { :controller => "director", :action => "create_row" })
+
+# READ
+get("/directors", { :controller => "directors", :action => "index" })
+get("/directors/:id", { :controller => "directors", :action => "show" })
+
+# UPDATE
+get("/directors/:id/edit_form", { :controller => "directors", :action => "edit_form" })
+get("/update_director/:id", { :controller => "directors", :action => "update_row" })
+
+# DELETE
+get("/delete_director/:id", { :controller => "directors", :action => "destroy" })
+
+
+# to access Movies
+# CREATE
+get("/movies/new", { :controller => "movies", :action => "new_form" })
+get("/create_movie", { :controller => "movies", :action => "create_row" })
+
+# READ
+get("/movies", { :controller => "movies", :action => "index" })
+get("/movies/:id", { :controller => "movies", :action => "show" })
+
+# UPDATE
+get("/movies/:id/edit_form", { :controller => "movies", :action => "edit_form" })
+get("/update_movie/:id", { :controller => "movies", :action => "update_row" })
+
+# DELETE
+get("/delete_movie/:id", { :controller => "movies", :action => "destroy_row" })
+
+
+# to access Actors
+# CREATE
+get('/actors/new_form', { :controller => 'actors', :action => 'new_form' })
+get('/create_actor', { :controller => 'actors', :action => 'create_row' })
+
+# READ
+get('/actors', { :controller => 'actors', :action => 'index' })
+get('/actors/:id', { :controller => 'actors', :action => 'show' })
+
+# UPDATE
+get('/actors/:id/edit_form', { :controller => 'actors', :action => 'edit_form' })
+get('/update_actor/:id', { :controller => 'actors', :action => 'update_row' })
+
+# DELETE
+get('/delete_actor/:id', { :controller => 'actors', :action => 'destroy' })  
+  
+  
+  
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
